8754d0ef3b59e43844d1e3a634f6e93dd1fb8505
JuanjoMoya <juanjo.moya1@gmail.com>
2017-02-24 15:08:56 +0100

    Add sign up proccess

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  #
+  # get 'users/show'
+  #
+  # get 'users/new'
+  #
+  # get 'users/create'
+  #
+  # get 'users/edit'
+  #
+  # get 'users/update'
+  #
+  # get 'users/destroy'
+
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
