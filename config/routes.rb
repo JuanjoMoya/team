dcad56c065fd81009f52b9fc27accc95d7d2125d
JuanjoMoya <juanjo.moya1@gmail.com>
2017-02-24 17:54:11 +0100

    Add sign in and sign out processes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # get 'users/index'
   #
   # get 'users/show'
@@ -15,6 +17,9 @@ Rails.application.routes.draw do
 
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
 
