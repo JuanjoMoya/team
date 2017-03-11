1cd1b103901130baeb0f2f9094d68a16b7bb2ad4
JuanjoMoya <juanjo.moya1@gmail.com>
2017-03-11 10:54:54 +0100

    Control access all groups for user admin

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,23 +1,30 @@
 class GroupsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+
+
   def index
-    # @groups = Group.paginate(page: params[:page])
-    @groups = current_user.groups.paginate(page: params[:page])
+    # @groups = current_user.groups.paginate(page: params[:page])
+    # binding.pry
+    if current_user.admin
+      @groups = Group.all.paginate(page: params[:page])
+    elsif
+      @groups = current_user.groups.paginate(page: params[:page])
+    end
   end
+
 
   def show
     @group = Group.find(params[:id])
     @message = current_user.messages.build if logged_in?
-    # @messages = @group.messages.paginate(page: params[:page])
-    # @messages = Message.@group.paginate(page: params[:page])
     @messages = Message.where("group_id = ?", @group).paginate(page: params[:page])
-
   end
+
 
   def new
     @user = User.all
     @group = Group.new
   end
+
 
   def create
     @group = Group.create(name: params[:group][:name])
@@ -33,25 +40,25 @@ class GroupsController < ApplicationController
     else
       render 'new'
     end
+
   end
+
 
   def edit
     @group = Group.find(params[:id])
     @users_ids = @group.users.map {|user| user.id }
   end
 
-  def update
 
+  def update
     @group = Group.find(params[:id])
     @group.users.delete_all
-
-
     @group.users << current_user
+
     params[:test].each do |user_id|
       user = User.find(user_id)
       @group.users << user
     end
-
 
     if @group.update_attributes(group_params)
       flash[:success] = "Group updated"
@@ -59,7 +66,9 @@ class GroupsController < ApplicationController
     else
       render 'edit'
     end
+
   end
+
 
   def destroy
     Group.find(params[:id]).destroy
@@ -67,9 +76,13 @@ class GroupsController < ApplicationController
     redirect_to groups_url
   end
 
+
   private
+
 
     def group_params
       params.require(:group).permit(:name)
     end
+
+
 end
